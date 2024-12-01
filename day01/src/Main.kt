fcc8acd84d2d81a823f95d26cae3c928395bf693
Alexander Voß <alexander.voss@it-voss.com>
2024-12-01 18:56:30 +0100

    day 1 completed

--- day01/src/Main.kt
+++ day01/src/Main.kt
@@ -1,19 +1,72 @@
 // (C) 2024 A.Voß, a.voss@fh-aachen.de, kotlin@codebasedlearning.dev
 
+import kotlin.math.abs
+import kotlin.time.measureTime
+
 const val day = 1
 
 fun main() {
     println("Day $day\n-----\n")
 
     val example1 = """
-a
-bb
-ccc
+3   4
+4   3
+2   5
+1   3
+3   9
+3   3
 """
-    val example1Lines = linesOf(data = example1)
-    // val inputLines = linesOf(day = day)
+    val inputData = linesOf(data = example1)
+    //val inputData = linesOf(day = day)
 
-    val inputData = example1Lines
-    // val inputData = inputLines
-    inputData.print(indent = 2, description = "input lines:", take = 2)
+    inputData.print(indent = 2, description = "input lines:", take = 6)
+
+    val lineRegex = """(\d+)\s+(\d+)""".toRegex()
+    // keep both lists at one place, no need for an extra class
+    val sortedData = Pair(mutableListOf<Int>(), mutableListOf<Int>()).apply {
+        for (line in inputData) {
+            lineRegex.matchEntire(line)!!.destructured.let { (n1, n2) ->
+                first.add(n1.toInt())
+                second.add(n2.toInt())
+            }
+        }
+        first.sort()
+        second.sort()
+    }
+
+    // Both parts are straightforward, so this is my shortest and most Kotlin-idiomatic solution with reasonable effort.
+    // Two comments: a collection modelling a sorted list may be tempting, but it does not make sense (imho),
+    // as the add=insert has to look for the right place to insert, but this is far more overhead than sorting the list
+    // at the end (in this situation here).
+    // Second, technically the second part does not need to work on the sorted lists, but if you really want to,
+    // you could simplify the counting by a) doing a binary search to find the first occurrence of a value
+    // in the second list, and then b) simply counting down and up as long as the elements are equal
+    // -> see example version 2.
+
+    // part 1: solutions: 11 / 2057374
+
+    val totalDistance = sortedData.run {
+        first.zip(second).sumOf { (n1, n2) -> abs(n1 - n2) }
+    }
+    println("part 1: total distance: $totalDistance")
+
+    // part 2: solutions: 31 / 23177084
+
+    val duration = measureTime {
+        val similarityScore = sortedData.run {
+            first.sumOf { n1 -> n1 * second.count { n2 -> n1 == n2 } }
+        }
+        println("part 2: similarity score: $similarityScore")
+    }
+    println("part 2: duration: $duration")
+
+    val durationV2 = measureTime {
+        val similarityScore = sortedData.run {
+            first.sumOf { n1 -> second.run { binarySearch(n1).let { index ->
+                if (index < 0) 0 else n1 * (1 + countWhile(index - 1, -1, n1) + countWhile(index + 1, 1, n1))
+            } } }
+        }
+        println("part 2: similarity score, version 2: $similarityScore")
+    }
+    println("part 2: duration version 2: $durationV2")
 }
